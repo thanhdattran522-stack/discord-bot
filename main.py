b65cdde012c5f0c13fd42f4bb26dd6ed1a75885e
thanhdattran522-stack <thanhdattran522@gmail.com>
2026-01-17 18:59:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,20 @@ import os
 import discord
 from discord.ext import commands
 
-TOKEN = os.getenv("TOKEN")
-
-if TOKEN is None:
-    print("❌ Không tìm thấy TOKEN")
-else:
-    print("✅ Đã load TOKEN")
-
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+TOKEN = os.getenv("TOKEN")
+
+if not TOKEN:
+    print("❌ Không tìm thấy TOKEN")
+    exit(1)
+
 @bot.event
 async def on_ready():
-    print(f"✅ Bot online: {bot.user}")
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send("🏓 Pong!")
+    print(f"✅ Bot đã online: {bot.user}")
 
 bot.run(TOKEN)
+
